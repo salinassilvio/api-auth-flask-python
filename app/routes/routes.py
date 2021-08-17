5b007a5cd995a2dc98be09243b632c2caba2d3fd
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2021-08-16 23:50:03 -0600

    Se Crea ruta de inicio de sesion login

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,5 +1,7 @@
+from types import resolve_bases
 import flask
 from flask import Blueprint,request,jsonify
+from flask_jwt_extended import create_access_token,get_jwt_identity,jwt_required
 from models.models import Usuario
 import bcrypt 
 blue_print = Blueprint('app',__name__)
@@ -39,3 +41,32 @@ def registrar_usuario():
 
     except Exception:
         return jsonify(respuesta='Error al crear usuario'),500
+
+#Ruta para Iniciar Sesion
+@blue_print.route('/auth/login',methods=['POST'])
+def iniciar_sesion():
+    try:
+         #Obtener el usuario
+        usuario = request.json.get('usuario')
+        #Obtener la Clave
+        clave = request.json.get('clave')
+        
+        if not usuario or not clave:
+            return jsonify(respuesta='Campos invalidos'),400
+
+        #consultar la BD
+        exist_usuario = Usuario.query.filter_by(usuario=usuario).first()
+
+        if not exist_usuario:
+            return jsonify(respuesta='Usuario no encontrado'),404
+
+        es_clave_valida = bcrypt.checkpw(clave.encode('utf-8'),exist_usuario.clave.encode('utf-8'))
+
+        #Se validan que las contraseñas sean iguales.
+        if es_clave_valida:
+            access_token = create_access_token(identity=usuario)
+            return jsonify(access_token=access_token),200
+        return jsonify(respuesta='Clave o Usuario Incorrecto'),404
+        
+    except Exception:
+        return jsonify(respuesta='Error en peticion'),500
